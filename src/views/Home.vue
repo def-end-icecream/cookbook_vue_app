684d85170af96133054f82f0f544f8e8952f6f49
dani <dani.zaghian@gmail.com>
2021-02-09 11:22:11 -0800

    adds axios library, and web request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
     <h1>All Recipes</h1>
-    <div v-for="recipe in recipes">
+    <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>Title: {{ recipe.title }}</h2>
+      <p>Chef: {{ recipe.chef }}</p>
+      <p>Ingredients: {{ recipe.ingredients }}</p>
+      <p>Directions: {{ recipe.directions }}</p>
       <p>Prep Time: {{ recipe.prep_time }}</p>
     </div>
   </div>
@@ -11,30 +14,25 @@
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
       message: "Hello World!",
-      recipes: [
-        {
-          id: 1,
-          title: "Raw Eggs",
-          prep_time: 5,
-        },
-        {
-          id: 2,
-          title: "Empty Sandwich",
-          prep_time: 10,
-        },
-        {
-          id: 3,
-          title: "Ice Cubes",
-          prep_time: 120,
-        },
-      ],
+      recipes: [],
     };
   },
-  created: function() {},
-  methods: {},
+  created: function() {
+    this.indexRecipes();
+  },
+  methods: {
+    indexRecipes: function() {
+      axios.get("http://localhost:3000/api/recipes").then((response) => {
+        console.log(response.data);
+        this.recipes = response.data;
+      });
+    },
+  },
 };
 </script>
