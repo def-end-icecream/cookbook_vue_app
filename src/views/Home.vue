505a9ee1c38b4bbed746bacb32c9877c71faa318
dani <dani.zaghian@gmail.com>
2021-02-09 10:30:29 -0800

    adds hardcoded recipes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>All Recipes</h1>
+    <div v-for="recipe in recipes">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Prep Time: {{ recipe.prep_time }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Hello World!",
+      recipes: [
+        {
+          id: 1,
+          title: "Raw Eggs",
+          prep_time: 5,
+        },
+        {
+          id: 2,
+          title: "Empty Sandwich",
+          prep_time: 10,
+        },
+        {
+          id: 3,
+          title: "Ice Cubes",
+          prep_time: 120,
+        },
+      ],
+    };
+  },
+  created: function() {},
+  methods: {},
+};
 </script>
