4119e8e4f49e118bc454d9124ff95e71b7344787
dani <dani.zaghian@gmail.com>
2021-02-10 09:32:49 -0800

    adds dynamic inputs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
     <h1>New Recipe</h1>
-    <button v-on:click="createRecipe()">Create</button>
+    <div>
+      Title: <input type="text" v-model="newRecipeTitle" /> <br />
+      Ingredients: <input type="text" v-model="newRecipeIngredients" /><br />
+      Directions: <input type="text" v-model="newRecipeDirections" /><br />
+      Prep Time: <input type="text" v-model="newRecipePrepTime" /><br />
+      Image Url: <input type="text" v-model="newRecipeImageUrl" /><br />
+      <button v-on:click="createRecipe()">Create</button>
+    </div>
+
     <h1>All Recipes</h1>
     <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>Title: {{ recipe.title }}</h2>
@@ -27,6 +35,11 @@ export default {
     return {
       message: "Hello World!",
       recipes: [],
+      newRecipeTitle: "",
+      newRecipeIngredients: "",
+      newRecipeDirections: "",
+      newRecipePrepTime: "",
+      newRecipeImageUrl: "",
     };
   },
   created: function() {
@@ -41,12 +54,11 @@ export default {
     },
     createRecipe: function() {
       var params = {
-        title: "Roasted Brussel Sprouts",
-        ingredients: "Brussels, olive oil, salt, pepper",
-        directions: "Roast the brussel sprouts",
-        prep_time: 20,
-        image_url:
-          "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/8/12/0/FN-Thanksgiving-2010_Brussels-Sprouts_s4x3.jpg.rend.hgtvcom.826.620.suffix/1384540892898.jpeg",
+        title: this.newRecipeTitle,
+        ingredients: this.newRecipeIngredients,
+        directions: this.newRecipeDirections,
+        prep_time: this.newRecipePrepTime,
+        image_url: this.newRecipeImageUrl,
       };
       axios
         .post("/api/recipes", params)
