094cfbbaabb7c86152e8641b22698e135bec84ee
dani <dani.zaghian@gmail.com>
2021-02-15 10:42:47 -0800

    adds restful recipes show page

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -2,7 +2,9 @@
   <div class="recipes-index">
     <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>Title: {{ recipe.title }}</h2>
-      <img v-bind:src="recipe.image_url" alt="" />
+      <router-link :to="`/recipes/${recipe.id}`">
+        <img v-bind:src="recipe.image_url" alt="" />
+      </router-link>
       <p>Ingredients: {{ recipe.ingredients }}</p>
       <p>Directions: {{ recipe.directions }}</p>
       <p>Prep Time: {{ recipe.prep_time }}</p>
---
+++ src/views/RecipesShow.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="recipes-show">
+    <h1>{{ recipe.title }}</h1>
+    <img :src="recipe.image_url" alt="" />
+    <p>Ingredients: {{ recipe.ingredients }}</p>
+    <p>Directions: {{ recipe.directions }}</p>
+    <p>Prep Time: {{ recipe.prep_time }}</p>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      recipe: {},
+    };
+  },
+  created: function() {
+    axios.get(`/api/recipes/${this.$route.params.id}`).then((response) => {
+      this.recipe = response.data;
+      console.log(this.recipe);
+    });
+  },
+  methods: {},
+};
+</script>
